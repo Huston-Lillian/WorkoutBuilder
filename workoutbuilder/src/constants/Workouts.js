3af4ff6b1e711b9cc689f48d4dcae8022d07caac
Daniel Garza <danielg@Daniels-MacBook-Pro.local>
2021-11-13 11:53:35 -0600

    Initial commit

---
+++ workoutbuilder/src/constants/Workouts.js
@@ -0,0 +1,57 @@
+export const exerciseObj = [
+  {
+    timeInSeconds: 5,
+    displayText: "30 seconds",
+    exerciseName: "Squats"
+  },
+  {
+    timeInSeconds: 5,
+    displayText: "30 seconds",
+    exerciseName: "Push Ups"
+  },
+  {
+    timeInSeconds: 5,
+    displayText: "30 seconds",
+    exerciseName: "Lunges"
+  },
+  {
+    timeInSeconds: 5,
+    displayText: "30 seconds",
+    exerciseName: "Standing Power Jacks"
+  },
+  {
+    timeInSeconds: 5,
+    displayText: "30 seconds",
+    exerciseName: "Squat Kicks"
+  },
+  {
+    timeInSeconds: 5,
+    displayText: "30 seconds",
+    exerciseName: "Tricep Dips"
+  },
+  {
+    timeInSeconds: 5,
+    displayText: "30 seconds",
+    exerciseName: "Leg Raises"
+  },
+  {
+    timeInSeconds: 5,
+    displayText: "30 seconds",
+    exerciseName: "Oblique Push Ups"
+  },
+  {
+    timeInSeconds: 5,
+    displayText: "30 seconds",
+    exerciseName: "Bicycle Abs"
+  },
+  {
+    timeInSeconds: 5,
+    displayText: "30 seconds",
+    exerciseName: "Plank"
+  },
+  {
+    timeInSeconds: 5,
+    displayText: "30 seconds",
+    exerciseName: "Rest"
+  }
+];
