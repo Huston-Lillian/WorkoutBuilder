c529387d3f8e898ae5ce96ecbcf6630404c28c01
Daniel Garza <danielg@Daniels-MacBook-Pro.local>
2021-11-14 12:20:39 -0600

    added social media to footer

--- workoutbuilder/src/App.js
+++ workoutbuilder/src/App.js
@@ -7,6 +7,11 @@ import background3 from "./pics/background3.jpg";
 import background4 from "./pics/background4.jpeg";
 import background5 from "./pics/background5.jpeg";
 import background6 from "./pics/background6.jpeg";
+import facebookIcon from "./pics/facebook_icon.png";
+import instaIcon from "./pics/insta_icon.png";
+import linkedinIcon from "./pics/linkedin_icon.png";
+import snapIcon from "./pics/snap_icon.png";
+import githubIcon from "./pics/github_icon.png";
 
 function App() {
   return (
@@ -221,7 +226,7 @@ function App() {
       <footer class="page-footer teal">
         <div class="container">
           <div class="row">
-            <div class="col l6 s12">
+            <div class="col l3 s12">
               <h5 class="white-text">About Me</h5>
               <p class="grey-text text-lighten-4">
                 My name is Daniel Garza and I'm a software engineer with a
@@ -253,9 +258,45 @@ function App() {
                 </li>
               </ul>
             </div> */}
-            <div class="col l3 s12">
+            <div class="col l1 s12"></div>
+            <div class="col l8 s12 left-align">
               <h5 class="white-text">Connect</h5>
-              <ul>
+              <div class="row center">
+                <div class="col s2 center">
+                  <a
+                    href="https://www.instagram.com/dan_the_realest_man/"
+                    target="_blank"
+                  >
+                    <img class="socialMedia-icon" src={instaIcon} />
+                  </a>
+                </div>
+                <div class="col s2 center">
+                  <a href="https://www.facebook.com/Dgza88" target="_blank">
+                    <img class="socialMedia-icon" src={facebookIcon} />
+                  </a>
+                </div>
+                <div class="col s2 center">
+                  <a
+                    href="https://www.snapchat.com/add/danielgarza8819"
+                    target="_blank"
+                  >
+                    <img class="socialMedia-icon" src={snapIcon} />
+                  </a>
+                </div>
+                <div class="col s2 center">
+                  <a href="https://www.linkedin.com/in/dgza88/" target="_blank">
+                    <img class="socialMedia-icon" src={linkedinIcon} />
+                  </a>
+                </div>
+
+                <div class="col s2 center">
+                  <a href="https://github.com/Dag2788" target="_blank">
+                    <img class="socialMedia-icon" src={githubIcon} />
+                  </a>
+                </div>
+              </div>
+
+              {/* <ul>
                 <li>
                   <a class="white-text" href="#!">
                     <i class="fa fa-facebook"></i>
@@ -276,7 +317,7 @@ function App() {
                     <i class="fa fa-linkedin"></i>
                   </a>
                 </li>
-              </ul>
+              </ul> */}
             </div>
           </div>
         </div>
