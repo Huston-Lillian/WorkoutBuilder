f0f9b8bc287a8dc4e0839c457b949f119ef2525c
Daniel Garza <danielg@Daniels-MacBook-Pro.local>
2021-11-16 11:47:09 -0600

    workouts page layout

--- workoutbuilder/public/js/init.js
+++ workoutbuilder/public/js/init.js
@@ -3,5 +3,10 @@
     $(".sidenav").sidenav();
     $(".parallax").parallax();
     $(".dropdown-trigger").dropdown();
+    $(".carousel").carousel({
+      indicators: true,
+      dist: 25,
+      shift: -3
+    });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
--- workoutbuilder/src/components/workouts.js
+++ workoutbuilder/src/components/workouts.js
@@ -2,6 +2,9 @@ import React from "react";
 import WorkoutComponent from "./WorkoutComponent";
 import background7 from "../pics/background7.jpeg";
 import bodyProject from "../pics/bodyProject.jpeg";
+import fitnessBlender from "../pics/fitnessBlender.jpeg";
+import madFit from "../pics/madFit.jpeg";
+import pernilla from "../pics/pernilla.jpeg";
 
 const styles = {
   transform: `translate3d(-50%, 298.213px, 0px)`,
@@ -16,7 +19,7 @@ function WorkoutsComponent() {
           <div class="container">
             <br></br>
             <div class="row center">
-              <h3 class=" blue-grey lighten-5 seeThroughWorkouts  col s4 black-text">
+              <h3 class=" blue-grey lighten-5 seeThroughWorkouts  col s6 black-text">
                 IT DOESN'T GET EASIER, YOU GET BETTER!
               </h3>
             </div>
@@ -35,36 +38,286 @@ function WorkoutsComponent() {
       <div class="container">
         <div class="section">
           <div class="row">
-            <div class="col s12 m4">
+            <div class="col s5 m4 paddingBottom">
               <div class="icon-block">
-                <h5 class="center">Team Body Project</h5>
+                <h5 class="center"></h5>
               </div>
-              <div class="s12 m4 center">
+              <div class="s4 m5 center">
                 <div class="light">
                   <img
-                    class="bodyProjectImg"
+                    class="bodyProjectImg circlePic"
                     src={bodyProject}
                     alt="Unsplashed background img 1"
                   />
                 </div>
               </div>
             </div>
+            <div class="col s7 m6">
+              <div class="carousel">
+                <iframe
+                  class="carousel-item"
+                  width="630"
+                  height="445"
+                  src="https://www.youtube.com/embed/x3aogIZfVUI"
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen="true"
+                ></iframe>
+                <iframe
+                  class="carousel-item"
+                  width="630"
+                  height="445"
+                  src="https://www.youtube.com/embed/x3aogIZfVUI"
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen="true"
+                ></iframe>
+                <iframe
+                  class="carousel-item"
+                  width="630"
+                  height="445"
+                  src="https://www.youtube.com/embed/x3aogIZfVUI"
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen="true"
+                ></iframe>
+                <iframe
+                  class="carousel-item"
+                  width="630"
+                  height="445"
+                  src="https://www.youtube.com/embed/x3aogIZfVUI"
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen="true"
+                ></iframe>
+              </div>
+              {/* <div class="col s4 m8">
+                <i class="material-icons large ">navigate_before</i>
+              </div>
+              <div class="col s4 m1"></div>
+              <div class="col s4 m3">
+                <i class="material-icons large ">navigate_next</i>
+              </div> */}
+            </div>
+          </div>
+        </div>
+      </div>
 
-            <div class="col s12 m8">
-              {/* <iframe
-                width="420"
-                height="345"
-                src="https://youtu.be/x3aogIZfVUI"
-              ></iframe> */}
-              <iframe
-                width="430"
-                height="345"
-                src="https://www.youtube.com/embed/x3aogIZfVUI"
-                title="YouTube video player"
-                frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen="true"
-              ></iframe>
+      <div class="container">
+        <div class="section">
+          <div class="row">
+            <div class="col s5 m4 paddingBottom">
+              <div class="icon-block">
+                <h5 class="center"></h5>
+              </div>
+              <div class="s4 m5 center">
+                <div class="light">
+                  <img
+                    class="bodyProjectImg circlePic"
+                    src={fitnessBlender}
+                    alt="Unsplashed background img 1"
+                  />
+                </div>
+              </div>
+            </div>
+            <div class="col s7 m6">
+              <div class="carousel">
+                <iframe
+                  class="carousel-item"
+                  width="630"
+                  height="445"
+                  src="https://www.youtube.com/embed/fKMGZW8cA1U"
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen="true"
+                ></iframe>
+                <iframe
+                  class="carousel-item"
+                  width="630"
+                  height="445"
+                  src="https://www.youtube.com/embed/fKMGZW8cA1U"
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen="true"
+                ></iframe>
+                <iframe
+                  class="carousel-item"
+                  width="630"
+                  height="445"
+                  src="https://www.youtube.com/embed/fKMGZW8cA1U"
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen="true"
+                ></iframe>
+                <iframe
+                  class="carousel-item"
+                  width="630"
+                  height="445"
+                  src="https://www.youtube.com/embed/fKMGZW8cA1U"
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen="true"
+                ></iframe>
+              </div>
+              {/* <div class="col s4 m8">
+                <i class="material-icons large ">navigate_before</i>
+              </div>
+              <div class="col s4 m1"></div>
+              <div class="col s4 m3">
+                <i class="material-icons large ">navigate_next</i>
+              </div> */}
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="container">
+        <div class="section">
+          <div class="row">
+            <div class="col s5 m4 paddingBottom">
+              <div class="icon-block">
+                <h5 class="center"></h5>
+              </div>
+              <div class="s4 m5 center">
+                <div class="light">
+                  <img
+                    class="bodyProjectImg circlePic"
+                    src={madFit}
+                    alt="Unsplashed background img 1"
+                  />
+                </div>
+              </div>
+            </div>
+            <div class="col s7 m6">
+              <div class="carousel">
+                <iframe
+                  class="carousel-item"
+                  width="630"
+                  height="445"
+                  src="https://www.youtube.com/embed/u3h_Yr40rhg"
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen="true"
+                ></iframe>
+                <iframe
+                  class="carousel-item"
+                  width="630"
+                  height="445"
+                  src="https://www.youtube.com/embed/u3h_Yr40rhg"
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen="true"
+                ></iframe>
+                <iframe
+                  class="carousel-item"
+                  width="630"
+                  height="445"
+                  src="https://www.youtube.com/embed/u3h_Yr40rhg"
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen="true"
+                ></iframe>
+                <iframe
+                  class="carousel-item"
+                  width="630"
+                  height="445"
+                  src="https://www.youtube.com/embed/u3h_Yr40rhg"
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen="true"
+                ></iframe>
+              </div>
+              {/* <div class="col s4 m8">
+                <i class="material-icons large ">navigate_before</i>
+              </div>
+              <div class="col s4 m1"></div>
+              <div class="col s4 m3">
+                <i class="material-icons large ">navigate_next</i>
+              </div> */}
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="container">
+        <div class="section">
+          <div class="row">
+            <div class="col s5 m4 paddingBottom">
+              <div class="icon-block">
+                <h5 class="center"></h5>
+              </div>
+              <div class="s4 m5 center">
+                <div class="light">
+                  <img
+                    class="bodyProjectImg circlePic"
+                    src={pernilla}
+                    alt="Unsplashed background img 1"
+                  />
+                </div>
+              </div>
+            </div>
+            <div class="col s7 m6">
+              <div class="carousel">
+                <iframe
+                  class="carousel-item"
+                  width="630"
+                  height="445"
+                  src="https://www.youtube.com/embed/KpKP_9l1ut0"
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen="true"
+                ></iframe>
+                <iframe
+                  class="carousel-item"
+                  width="630"
+                  height="445"
+                  src="https://www.youtube.com/embed/KpKP_9l1ut0"
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen="true"
+                ></iframe>
+                <iframe
+                  class="carousel-item"
+                  width="630"
+                  height="445"
+                  src="https://www.youtube.com/embed/KpKP_9l1ut0"
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen="true"
+                ></iframe>
+                <iframe
+                  class="carousel-item"
+                  width="630"
+                  height="445"
+                  src="https://www.youtube.com/embed/KpKP_9l1ut0"
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen="true"
+                ></iframe>
+              </div>
+              {/* <div class="col s4 m8">
+                <i class="material-icons large ">navigate_before</i>
+              </div>
+              <div class="col s4 m1"></div>
+              <div class="col s4 m3">
+                <i class="material-icons large ">navigate_next</i>
+              </div> */}
             </div>
           </div>
         </div>
