3e9e9e462c45adc090922aaf2859ca116034ed23
Daniel Garza <danielg@Daniels-MacBook-Pro.local>
2021-11-15 19:15:05 -0600

    navbar routing

--- workoutbuilder/public/js/init.js
+++ workoutbuilder/public/js/init.js
@@ -2,5 +2,6 @@
   $(function() {
     $(".sidenav").sidenav();
     $(".parallax").parallax();
+    $(".dropdown-trigger").dropdown();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
