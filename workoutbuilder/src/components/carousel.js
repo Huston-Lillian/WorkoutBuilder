6e82d5055bf7899d2511ebdb1e7d538dde5b4a56
Daniel Garza <danielg@Daniels-MacBook-Pro.local>
2021-11-20 16:55:26 -0600

    carousel improvement

---
+++ workoutbuilder/src/components/carousel.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import styles from "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import { youTubeLinks } from "../constants/youtubelinks";
+
+type Props = {
+  id: number
+};
+
+class DemoCarousel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      //   links: youtubeLinks[this.props.id]
+      links: youTubeLinks[this.props.id]
+    };
+    document.querySelector(".demo-carousel");
+  }
+
+  render() {
+    return (
+      <Carousel>
+        {this.state.links.map(link => (
+          <div class="paddingBottom">
+            <iframe
+              width="480"
+              height="300"
+              src={link}
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen="true"
+              class="responsive-iframe"
+            ></iframe>
+          </div>
+        ))}
+      </Carousel>
+    );
+  }
+}
+export default DemoCarousel;
+//ReactDOM.render(<DemoCarousel />, document.querySelector(".demo-carousel"));
+
+// Don't forget to include the css in your page
+
+// Using webpack or parcel with a style loader
+// import styles from 'react-responsive-carousel/lib/styles/carousel.min.css';
+
+// Using html tag:
+// <link rel="stylesheet" href="<NODE_MODULES_FOLDER>/react-responsive-carousel/lib/styles/carousel.min.css"/>
