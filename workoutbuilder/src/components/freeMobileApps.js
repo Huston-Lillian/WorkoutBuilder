5be1c4b557c70e0a03a069adacc756f1b33a4cb7
Lillian Huston <Huston.lillian@yahoo.com>
2021-12-30 12:10:55 -0600

    added freeMobileApps

---
+++ workoutbuilder/src/components/freeMobileApps.js
@@ -0,0 +1,115 @@
+import React, { Component } from "react";
+import { feeAppsArray } from "../constants/freeApps";
+import background17 from "../pics/background17.jpg";
+import Googleplay from "../pics/Googleplay.png";
+import AppleStore from "../pics/AppleStore.png";
+
+const styles = {
+  transform: `translate3d(-50%, 298.213px, 0px)`,
+  opacity: 1,
+};
+
+class FreeMobileAppsComponent extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      freeApps: feeAppsArray,
+    };
+  }
+  render() {
+    let { freeApps } = this.state;
+    window.scrollTo(0, 0);
+    return (
+      <React.Fragment>
+        <div id="index-banner" class="parallax-container">
+          <div class="section no-pad-bot">
+            <div class="container">
+              <br></br>
+              <br></br>
+            </div>
+          </div>
+          <div class="parallax">
+            <img
+              src={background17}
+              alt="Unsplashed background img 1"
+              style={styles}
+            />
+          </div>
+        </div>
+        <div className="container">
+          <br></br>
+          <div className="row center paddingBottom">
+            <h1 className="center header teal-text text-lighten-2">
+              Free Fitness Apps
+            </h1>
+            <br></br>
+            <br></br>
+          </div>
+        </div>
+        {freeApps.map((app) => (
+          <div class="container">
+            <div class="section">
+              <div class="row">
+                <div class="col s12 m4 customWorkoutImg">
+                  <div class="icon-block">
+                    <h5 class="center"></h5>
+                  </div>
+                  <div class="s4 m5 center">
+                    <div class="light">
+                      {/* <a href={app.insta}> */}
+                      <img
+                        class="adviceImg circlePic"
+                        src={app.appImage}
+                        alt="Unsplashed background img 1"
+                      />
+                      {/* </a> */}
+                    </div>
+                  </div>
+                </div>
+                <div class="col s12 m6">
+                  <div class="row center appNames">
+                    <h3 class="black-text">{app.appName}</h3>
+                  </div>
+                  <div class="row">
+                    <div class="col s12 m6 center">
+                      <div class="light">
+                        <a href={app.googlePlayLink}>
+                          <img
+                            className="appLogos"
+                            src={Googleplay}
+                            alt="Unsplashed background img 1"
+                            href={app.googlePlayLink}
+                          />
+                        </a>
+                      </div>
+                    </div>
+                    <div class="col s12 m6 center">
+                      <div class="light">
+                        <a href={app.appleStoreLink}>
+                          <img
+                            className="appLogos"
+                            src={AppleStore}
+                            alt="Unsplashed background img 1"
+                            href={app.appleStoreLink}
+                          />
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+      </React.Fragment>
+    );
+  }
+}
+
+export default FreeMobileAppsComponent;
---
+++ workoutbuilder/src/constants/freeApps.js
@@ -0,0 +1,164 @@
+import fiton from "../pics/fiton.png";
+import workoutTrainer from "../pics/workoutTrainer.png";
+import dailyWorkouts from "../pics/dailyWorkouts.png";
+import sworkit from "../pics/sworkit.png";
+import jefit from "../pics/jefit.png";
+import dailyYoga from "../pics/dailyYoga.png";
+import sevenMinute from "../pics/sevenMinute.webp";
+import addidas from "../pics/addidas.png";
+import nikeTraining from "../pics/nikeTraining.png";
+import mapMyFitness from "../pics/mapMyFitness.png";
+import freeletics from "../pics/freeletics.png";
+import classPass from "../pics/classPass.png";
+import dailyButt from "../pics/dailyButt.png";
+import asana from "../pics/asana.jpeg";
+import aaptiv from "../pics/aaptiv.png";
+import eightFit from "../pics/eightFit.webp";
+import fitify from "../pics/fitify.webp";
+import fiit from "../pics/fiit.png";
+
+export const feeAppsArray = [
+  {
+    appImage: fiton,
+    appName: "FitOn",
+    googlePlayLink:
+      "https://play.google.com/store/apps/details?id=com.fiton.android&hl=en_US&gl=US",
+    appleStoreLink:
+      "https://apps.apple.com/us/app/fiton-workouts-fitness-plans/id1442473191",
+  },
+  {
+    appImage: workoutTrainer,
+    appName: "Workout Trainer",
+    googlePlayLink:
+      "https://play.google.com/store/apps/details?id=com.skimble.workouts&hl=en_US&gl=US",
+    appleStoreLink:
+      "https://apps.apple.com/us/app/workout-trainer-fitness-coach/id395686735",
+  },
+  {
+    appImage: dailyWorkouts,
+    appName: "Daily Workouts FREE",
+    googlePlayLink:
+      "https://play.google.com/store/apps/details?id=com.tinymission.dailyworkoutsfree&hl=en_US&gl=US",
+    appleStoreLink:
+      "https://apps.apple.com/us/app/daily-workouts-home-trainer/id469068059",
+  },
+  {
+    appImage: sworkit,
+    appName: "Sworkit",
+    googlePlayLink:
+      "https://play.google.com/store/apps/details?id=sworkitapp.sworkit.com&hl=en_US&gl=US",
+    appleStoreLink:
+      "https://apps.apple.com/us/app/sworkit-fitness-workout-app/id527219710",
+  },
+  {
+    appImage: jefit,
+    appName: "JEFIT",
+    googlePlayLink:
+      "https://play.google.com/store/apps/details?id=je.fit&hl=en_US&gl=US",
+    appleStoreLink:
+      "https://apps.apple.com/us/app/jefit-workout-planner-gym-log/id449810000",
+  },
+  {
+    appImage: dailyYoga,
+    appName: "Daily Yoga",
+    googlePlayLink:
+      "https://play.google.com/store/apps/details?id=com.dailyyoga.inc&hl=en_US&gl=US",
+    appleStoreLink:
+      "https://apps.apple.com/us/app/yoga-daily-yoga-for-everyone/id545849922",
+  },
+  {
+    appImage: sevenMinute,
+    appName: "7 Minute Workout",
+    googlePlayLink:
+      "https://play.google.com/store/apps/details?id=com.popularapp.sevenmins&hl=en_US&gl=US",
+    appleStoreLink:
+      "https://apps.apple.com/us/app/7-minute-workout/id650762525",
+  },
+  {
+    appImage: addidas,
+    appName: "Addidas Training by Runtastic",
+    googlePlayLink:
+      "https://play.google.com/store/apps/details?id=com.runtastic.android.results.lite&hl=en_US&gl=US",
+    appleStoreLink:
+      "https://apps.apple.com/us/app/adidas-training-by-runtastic/id1035263816",
+  },
+  {
+    appImage: nikeTraining,
+    appName: "Nike Training Club",
+    googlePlayLink:
+      "https://play.google.com/store/apps/details?id=com.nike.ntc&hl=en_US&gl=US",
+    appleStoreLink:
+      "https://apps.apple.com/us/app/nike-training-club/id301521403",
+  },
+  {
+    appImage: mapMyFitness,
+    appName: "Map my Fitness by Underarmour",
+    googlePlayLink:
+      "https://play.google.com/store/apps/details?id=com.mapmyfitness.android2&hl=en_US&gl=US",
+    appleStoreLink:
+      "https://apps.apple.com/us/app/map-my-fitness-by-under-armour/id298903147",
+  },
+  {
+    appImage: freeletics,
+    appName: "Freeletics",
+    googlePlayLink:
+      "https://play.google.com/store/apps/details?id=com.freeletics.lite&hl=en_US&gl=US",
+    appleStoreLink:
+      "https://apps.apple.com/us/app/freeletics-hiit-fitness-coach/id654810212",
+  },
+  {
+    appImage: classPass,
+    appName: "ClassPass",
+    googlePlayLink:
+      "https://play.google.com/store/apps/details?id=com.classpass.classpass&hl=en_US&gl=US",
+    appleStoreLink:
+      "https://apps.apple.com/us/app/classpass-fitness-workout-app/id912947244",
+  },
+  {
+    appImage: dailyButt,
+    appName: "Daily Butt Workout",
+    googlePlayLink:
+      "https://play.google.com/store/apps/details?id=com.tinymission.dailybuttworkoutfree&hl=en_US&gl=US",
+    appleStoreLink:
+      "https://apps.apple.com/gb/app/daily-butt-workout-trainer/id398552083",
+  },
+  {
+    appImage: asana,
+    appName: "Asana Rebel",
+    googlePlayLink:
+      "https://play.google.com/store/apps/details?id=com.asanayoga.asanarebel&hl=en_US&gl=US",
+    appleStoreLink:
+      "https://apps.apple.com/us/app/asana-rebel-get-in-shape/id1067860796",
+  },
+  {
+    appImage: aaptiv,
+    appName: "Aaptiv",
+    googlePlayLink:
+      "https://play.google.com/store/apps/details?id=com.aaptiv.android&hl=en_US&gl=US",
+    appleStoreLink:
+      "https://apps.apple.com/us/app/aaptiv-1-audio-fitness-app/id869058995",
+  },
+  {
+    appImage: eightFit,
+    appName: "8 Fit",
+    googlePlayLink:
+      "https://play.google.com/store/apps/details?id=com.eightfit.app",
+    appleStoreLink:
+      "https://apps.apple.com/us/app/8fit-workouts-meal-planner/id866617777",
+  },
+  {
+    appImage: fitify,
+    appName: "Kettlebell Workouts by Fitify",
+    googlePlayLink:
+      "https://play.google.com/store/apps/details?id=com.fitifyapps.kettlebell",
+    appleStoreLink:
+      "https://apps.apple.com/us/app/kettlebell-workouts-by-fitify/id1199489169",
+  },
+  {
+    appImage: fiit,
+    appName: "FIIT",
+    googlePlayLink: "https://play.google.com/store/apps/details?id=tv.fiit.app",
+    appleStoreLink:
+      "https://apps.apple.com/us/app/fiit-workouts-fitness-plans/id1296861162",
+  },
+];
